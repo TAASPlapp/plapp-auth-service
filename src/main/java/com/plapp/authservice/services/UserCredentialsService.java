8231dbe4e0ceb7e6d39c93349a6c57a79bd7865e
Carlo Alberto Barbano <carlo.alberto.barbano@outlook.com>
2020-02-26 23:09:16 +0100

    move business logic to service

--- src/main/java/com/plapp/authservice/services/UserCredentialsService.java
+++ src/main/java/com/plapp/authservice/services/UserCredentialsService.java
@@ -1,23 +1,31 @@
 package com.plapp.authservice.services;
 
-import com.plapp.authservice.repositories.UserCredentialsRepository;
+import com.plapp.authservice.security.JWTAuthenticationManager;
 import com.plapp.entities.auth.UserCredentials;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.plapp.authservice.repositories.UserCredentialsRepository;
+import io.jsonwebtoken.Claims;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 
 @Service
+@RequiredArgsConstructor
 public class UserCredentialsService implements UserDetailsService {
+    private final UserCredentialsRepository userCredentialsRepository;
+    private final BCryptPasswordEncoder passwordEncoder;
+    private final AuthenticationManager authenticationManager;
+    private final JWTAuthenticationManager jwtAuthenticationManager;
 
-    @Autowired
-    UserCredentialsRepository userCredentialsRepository;
-
-    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+    public User loadUserByUsername(String email) throws UsernameNotFoundException {
         UserCredentials credentials = userCredentialsRepository.findByEmail(email);
 
         if (credentials == null) {
@@ -25,5 +33,32 @@ public class UserCredentialsService implements UserDetailsService {
         }
 
         return new User(credentials.getEmail(), credentials.getPassword(), new ArrayList<>());
+    }
+
+    public UserCredentials createUser(UserCredentials userCredentials) {
+        if (userCredentialsRepository.findByEmail(userCredentials.getEmail()) != null)
+            throw new IllegalArgumentException("Email already exists");
+
+        userCredentials.setPassword(passwordEncoder.encode(userCredentials.getPassword()));
+        return userCredentialsRepository.save(userCredentials);
+    }
+
+    public String authenticateUser(UserCredentials credentials) throws UsernameNotFoundException,
+                                                                       BadCredentialsException {
+        if (credentials.getPassword() == null || credentials.getEmail() == null)
+            throw new IllegalArgumentException("Missing credentials");
+
+
+        Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(
+                credentials.getEmail(),
+                credentials.getPassword()
+        ));
+
+        UserCredentials existingUser = userCredentialsRepository.findByEmail(credentials.getEmail());
+        return jwtAuthenticationManager.buildJWT(existingUser);
+    }
+
+    public Claims verifyJWT(String jwt) {
+        return jwtAuthenticationManager.verifyJWT(jwt);
     }
 }
