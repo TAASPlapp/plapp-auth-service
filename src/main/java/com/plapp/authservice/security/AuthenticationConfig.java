ff258d9354572a6c66120aa4b8e1c74b1635e068
Carlo Alberto Barbano <carlo.alberto.barbano@outlook.com>
2020-03-03 21:12:23 +0100

    clean imports

--- src/main/java/com/plapp/authservice/security/AuthenticationConfig.java
+++ src/main/java/com/plapp/authservice/security/AuthenticationConfig.java
@@ -4,7 +4,6 @@ import com.plapp.authorization.AuthorizationConfig;
 import com.plapp.authorization.JWTAuthorizationRegexFilter;
 import com.plapp.authservice.services.SpringUserService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
@@ -12,7 +11,6 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
